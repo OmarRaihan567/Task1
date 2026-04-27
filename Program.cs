3e1ef3409d6cd69ed535f205dbd59428b17b5e8f
OmarRaihan <omarr712007@gmail.com>
2026-04-27 17:26:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -28,14 +28,21 @@ namespace Task1
             // Welcome Message 
             Console.WriteLine("-- Welcome to Islam's Carpets Cleaning Service --");
 
+            //Price List
+            Console.WriteLine("- The price list -");
+            Console.WriteLine("Small Carpets: $25");
+            Console.WriteLine("Large Carpets: $35");
+            Console.WriteLine("Tax: 6%");
+
+
             // User enters the number of each size of carpets he wants to clean
-            Console.Write("Enter the number of small carpets you want to clean: ");
+            Console.Write("Please enter the number of small carpets you want to clean: ");
             int SCC = Convert.ToInt32(Console.ReadLine()); // SCC -> SmallCarpetsCount
             
-            Console.Write("Enter the number of Large carpets you want to clean: ");
+            Console.Write("Please enter the number of Large carpets you want to clean: ");
             int LCC = Convert.ToInt32(Console.ReadLine()); // LCC -> LargeCarpetsCount
 
-            // initalizing Cost & Tax & Total estimate variables
+            // initalizing Cost, Tax and Total estimate variables
             int Cost = (SCC * 25) + (LCC * 35);
             double Tax = 0.06 * Cost;
             double Total_estimate = Cost + Tax;
@@ -57,6 +64,7 @@ namespace Task1
             Console.WriteLine($"Total estimate: {Total_estimate}");
             Console.WriteLine("This estimate is valid for 30 days");
 
+            // Farewell Message
             Console.WriteLine("- Thank you for dealing with us! -");
             
         }
